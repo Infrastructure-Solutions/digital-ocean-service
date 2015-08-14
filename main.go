3d1c734e4048e0f849093fabaefb25b2ecd55a0e
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2015-08-14 16:41:33 -0500

    Remove Keys

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ func main() {
 
 	handler := interfaces.WebServiceHandler{
 		Interactor:  doInteractor,
-		ID:          "f584247261a56d4003d795842fbeaacdd82d5624693bd6e00c02b3c6d675cf44",
-		Secret:      "9f515e7485d3d8e602b726620fe115084ff95f641228285901bd1959a350c05c",
+		ID:          "",
+		Secret:      "",
 		RedirectURI: "http://localhost:7000/do_callback",
 	}
 
