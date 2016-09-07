3938513af180955755d82687858a1dcfb88ffa1b
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2016-09-07 14:46:21 -0500

    Standarize the use of Key and update droplet to reflect the new fields

--- domain/droplet.go
+++ domain/droplet.go
@@ -1,34 +1,32 @@
 package domain
 
 type DropletRequest struct {
-	Name              string                `json:"name"`
-	Region            string                `json:"region"`
-	Size              string                `json:"size"`
-	Image             string                `json:"image"`
-	Backups           bool                  `json:"backups"`
-	IPv6              bool                  `json:"ipv6"`
-	PrivateNetworking bool                  `json:"private_networking"`
-	UserData          string                `json:"user_data,omitempty"`
-	SSHKeys           []DropletCreateSSHKey `json:"ssh_keys"`
-}
-
-type DropletCreateSSHKey struct {
-	ID          int    `json:"id"`
-	Fingerprint string `json:"fingerprint"`
+	Name              string `json:"name"`
+	Region            string `json:"region"`
+	Size              string `json:"size"`
+	Image             string `json:"image"`
+	Backups           bool   `json:"backups"`
+	IPv6              bool   `json:"ipv6"`
+	PrivateNetworking bool   `json:"private_networking"`
+	UserData          string `json:"user_data,omitempty"`
+	SSHKeys           []Key  `json:"ssh_keys"`
 }
 
 type Droplet struct {
-	ID       int        `json:"id"`
-	Name     string     `json:"name"`
-	Region   string     `json:"region"`
-	Size     string     `json:"size"`
-	UserData string     `json:"user_data,omitempty"`
-	Networks []Networks `json:"networks"`
+	ID                int      `json:"id"`
+	Name              string   `json:"name"`
+	Region            string   `json:"region"`
+	InstanceName      string   `json:"size"`
+	OperatingSystem   string   `json:"operating_system"`
+	IPV6              string   `json:"ipv6,omitempty"`
+	PrivateNetworking bool     `json:"private_networking"`
+	Networks          Networks `json:"networks"`
+	SSHKeys           []Key    `json:"ssh_keys"`
 }
 
 type Networks struct {
-	V4 []NetworkV4 `json:"v4,omitempty"`
-	V6 []NetworkV6 `json:"v6,omitempty"`
+	V4 []NetworkV4 `json:"v4"`
+	V6 []NetworkV6 `json:"v6"`
 }
 
 type NetworkV6 struct {
