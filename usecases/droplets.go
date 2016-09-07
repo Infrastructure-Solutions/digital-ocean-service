3b1b53d0921526de4b5181d2314c90a8481b8524
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2016-09-07 14:47:56 -0500

    Add missing fields

--- usecases/droplets.go
+++ usecases/droplets.go
@@ -6,7 +6,12 @@ import (
 	"github.com/jinzhu/copier"
 )
 
-func (interactor DOInteractor) CreateDroplet(droplet domain.DropletRequest, token string) (*domain.Droplet, error) {
+type Instance struct {
+	Provider string `json:"provider"`
+	domain.Droplet
+}
+
+func (interactor DOInteractor) CreateDroplet(droplet domain.DropletRequest, token string) (*Instance, error) {
 	client := getClient(token)
 
 	dropletRequest := &godo.DropletCreateRequest{
@@ -33,15 +38,21 @@ func (interactor DOInteractor) CreateDroplet(droplet domain.DropletRequest, toke
 	dropletRequest.SSHKeys = sshkeys
 
 	drop, _, err := client.Droplets.Create(dropletRequest)
+
 	if err != nil {
 		return nil, err
 	}
 
-	drp := &domain.Droplet{
-		Name:     droplet.Name,
-		Region:   droplet.Region,
-		Size:     droplet.Size,
-		UserData: droplet.UserData,
+	inst := &Instance{
+		Provider: "digital_ocean",
+		Droplet: domain.Droplet{
+			ID:                drop.ID,
+			Name:              droplet.Name,
+			Region:            droplet.Region,
+			OperatingSystem:   drop.Image.Slug,
+			PrivateNetworking: false,
+			InstanceName:      drop.Size.Slug,
+		},
 	}
 
 	networksV4 := []domain.NetworkV4{}
@@ -57,8 +68,14 @@ func (interactor DOInteractor) CreateDroplet(droplet domain.DropletRequest, toke
 		copier.Copy(n, net)
 		networksV6 = append(networksV6, n)
 	}
+	networks := domain.Networks{
+		V4: networksV4,
+		V6: networksV6,
+	}
 
-	return drp, nil
+	inst.Networks = networks
+	inst.SSHKeys = droplet.SSHKeys
+	return inst, nil
 
 }
 
@@ -74,9 +91,9 @@ func (interactor DOInteractor) ListDroplets(token string) ([]domain.Droplet, err
 
 	for _, drops := range doDrops {
 		drp := domain.Droplet{
-			Name:   drops.Name,
-			Region: drops.Region.String(),
-			Size:   drops.Size.String(),
+			Name:         drops.Name,
+			Region:       drops.Region.String(),
+			InstanceName: drops.Size.String(),
 		}
 
 		networksV4 := []domain.NetworkV4{}
