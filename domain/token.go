81f1e1fce29dda4851a4604ceeddff57865d93f6
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2016-09-08 20:34:03 -0500

    Add domain documentation

--- domain/token.go
+++ domain/token.go
@@ -1,5 +1,6 @@
 package domain
 
+// DOToken is the response of the OAUTH for Digital Ocean
 type DOToken struct {
 	AccessToken  string `json:"access_token"`
 	TokenType    string `json:"token_type"`
@@ -9,6 +10,7 @@ type DOToken struct {
 	Info         Info   `json:"info"`
 }
 
+// Info is the user information in a OAUTH requests from Digital Ocean
 type Info struct {
 	Name  string `json:"name"`
 	Email string `json:"email"`
