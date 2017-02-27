2dbe7885c3cb132262588ee55b6b50bbb99c45e1
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2017-02-27 12:55:03 -0600

    Add the route which will use the adapter

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"net/http"
 
 	"github.com/Tinker-Ware/digital-ocean-service/infrastructure"
 	"github.com/Tinker-Ware/digital-ocean-service/interfaces"
@@ -52,6 +53,7 @@ func main() {
 	subrouter.HandleFunc("/digital_ocean/instances", handler.CreateDroplet).Methods("POST")
 	subrouter.HandleFunc("/digital_ocean/instances", handler.ListDroplets).Methods("GET")
 	subrouter.HandleFunc("/digital_ocean/instance/{instanceID}", handler.GetInstance).Methods("GET")
+	subrouter.Handle("/digital_ocean/keys", interfaces.Adapt(http.HandlerFunc(handler.CreateKey), interfaces.GetToken(config.APIHost, config.Salt))).Methods("POST")
 
 	n := negroni.Classic()
 	n.UseHandler(handlers.CORS(headers, origins)(r))
